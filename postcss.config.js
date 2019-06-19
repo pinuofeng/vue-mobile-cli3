2e9a78925808a9010b7eb889dae167a14aa40479
黄小峰 <724332274@qq.com>
2019-06-19 10:03:15 +0800

    commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {}
-  }
+    plugins: {
+        autoprefixer: {},
+        'postcss-pxtorem': {
+            /*
+            *
+            * 逻辑像素(pt) / 物理像素(px)
+            * 320pt = 640px 、375pt = 750px
+            * 设计图是按物理像素来设计的，所以如果设计图是640物理像素宽度，rootValue就设置为32.0
+            * 同理如果设计图是750物理像素宽度，rootValue就设置为37.5，总之，设计图要除以2。
+            *
+            * 下面设置的37.5，意思就是按37.5px = 1rem进行px的转换
+            *
+            */
+            rootValue: 37.5,
+            propList: ['*'],
+            // 该项仅在使用Vant的 Circle 组件时需要
+            // 该属性配置表示不转换数组里的类名为rem
+            selectorBlackList: ['van-circle__layer']
+        }
+    }
 }
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const webpack = require('webpack');
 const path = require('path');
-const autoprefixer = require('autoprefixer');
-const pxtorem = require('postcss-pxtorem');
 const settings = require('./src/settings');
 
 function resolve (dir) {
@@ -30,34 +28,6 @@ module.exports =  {
         port: settings.SERVER_PORT,
         open: settings.AUTO_OPEN_BROWSER,
         //proxy: settings.PROXY_SERVERS
-    },
-
-    // cssLoader配置，自动加浏览器前缀、px转rem
-    css: {
-        loaderOptions: {
-            postcss: {
-                plugins: [
-                    autoprefixer(),
-                    pxtorem({
-                        /*
-                        *
-                        * 逻辑像素(pt) / 物理像素(px)
-                        * 320pt = 640px 、375pt = 750px
-                        * 设计图是按物理像素来设计的，所以如果设计图是640物理像素宽度，rootValue就设置为32.0
-                        * 同理如果设计图是750物理像素宽度，rootValue就设置为37.5，总之，设计图要除以2。
-                        *
-                        * 下面设置的37.5，意思就是按37.5px = 1rem进行px的转换
-                        *
-                        */
-                        rootValue: 37.5,
-                        propList: ['*'],
-                        // 该项仅在使用 Circle 组件时需要
-                        // 不转换数组里的类名
-                        selectorBlackList: ['van-circle__layer']
-                    })
-                ]
-            }
-        }
     },
 
     // webpack配置
